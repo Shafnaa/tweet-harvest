5de31f68feaf2c4029a30c946240d27010f72516
Shafnaa <saujanashafi2003@gmail.com>
2025-08-03 17:09:51 +0700

    fix: Fix emoji

--- src/crawl.ts
+++ src/crawl.ts
@@ -297,14 +297,14 @@ export async function crawl({
 
             // replace all emojis
             // Emoji regex pattern
-            const emojiPattern =
-              /[\u{1F600}-\u{1F64F}\u{1F300}-\u{1F5FF}\u{1F680}-\u{1F6FF}\u{1F700}-\u{1F77F}\u{1F780}-\u{1F7FF}\u{1F800}-\u{1F8FF}\u{1F900}-\u{1F9FF}\u{1FA00}-\u{1FA6F}\u{1FA70}-\u{1FAFF}\u{2600}-\u{26FF}\u{2700}-\u{27BF}]/gu;
+            // const emojiPattern =
+            //   /[\u{1F600}-\u{1F64F}\u{1F300}-\u{1F5FF}\u{1F680}-\u{1F6FF}\u{1F700}-\u{1F77F}\u{1F780}-\u{1F7FF}\u{1F800}-\u{1F8FF}\u{1F900}-\u{1F9FF}\u{1FA00}-\u{1FA6F}\u{1FA70}-\u{1FAFF}\u{2600}-\u{26FF}\u{2700}-\u{27BF}]/gu;
 
             // Replace all instances of emojis in the string
-            cleanTweetText = cleanTweetText.replace(emojiPattern, "");
+            // cleanTweetText = cleanTweetText.replace(emojiPattern, "");
 
             // replace all double spaces with single space
-            cleanTweetText = cleanTweetText.replace(/\s\s+/g, " ");
+            // cleanTweetText = cleanTweetText.replace(/\s\s+/g, " ");
 
             if (IS_DETAIL_MODE) {
               const firstWord = cleanTweetText.split(" ")[0];
